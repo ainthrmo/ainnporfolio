05f89ca16d1a2940436ee7a1e00b1b713c1ea346
AindraMoh <contact@aindramoh.dev>
2026-01-12 12:06:04 +0630

    Initial project setup with layout skeleton

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Your Name</h1>
+      <ul>
+        <li>Projects</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
